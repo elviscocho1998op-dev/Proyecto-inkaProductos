aa0aaa7cafa201e35306e16a6d624d8cd56db254
jelias <elviscocho1998op@gmail.com>
2025-12-20 05:19:54 -0500

    Update-v1.2

--- inka-productos/src/main/java/pe/cibertec/inkaproductos/repositories/MovimientoRepository.java
+++ inka-productos/src/main/java/pe/cibertec/inkaproductos/repositories/MovimientoRepository.java
@@ -1,7 +1,11 @@
 package pe.cibertec.inkaproductos.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import pe.cibertec.inkaproductos.models.Movimiento;
 
+import java.util.List;
+
 public interface MovimientoRepository extends JpaRepository<Movimiento, Integer> {
+
 }
